54bf2b920ce4ed01a047708dda951a0ec50679fc
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-05-31 21:36:52 +0300

    get_size

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ from kivy.app import App
 from kivy.properties import ColorProperty
 from kivy.uix.button import Button
 
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', '0')
+Config.set('graphics', 'width', '300')
+Config.set('graphics', 'height', '610')
+
+
 class MyApp(App):
     def build(self):
         return Button(
             text="Кнопка",
             font_size=25,
             on_press =self.pushbutton,
-            background_color = [1, 0, 0, 1],
+            background_color = [.42,.32,.55, 1],
             background_normal = ''
         )
 
