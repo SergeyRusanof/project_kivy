0417974a3807bd36b81079e2cd74585131fa02b8
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-06-03 11:49:12 +0300

    get_size2

--- main.py
+++ main.py
@@ -7,8 +7,9 @@
 from kivy.app import App
 from kivy.properties import ColorProperty
 from kivy.uix.button import Button
-
 from kivy.config import Config
+
+from kivy.uix.floatlayout import FloatLayout
 
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '300')
@@ -17,13 +18,17 @@ Config.set('graphics', 'height', '610')
 
 class MyApp(App):
     def build(self):
-        return Button(
-            text="Кнопка",
-            font_size=25,
-            on_press =self.pushbutton,
-            background_color = [.42,.32,.55, 1],
-            background_normal = ''
-        )
+        fl = FloatLayout(size=(150, 150))
+        fl.add_widget(
+            Button(
+                text="Кнопка",
+                font_size=16,
+                on_press=self.pushbutton,
+                background_color=[.42, .32, .55, 1],
+                background_normal='',
+                size_hint = (.5, .25),
+                pos = (0, 0)))
+        return fl
 
     def pushbutton(self, instance):
         print('Кнопка нажата')
