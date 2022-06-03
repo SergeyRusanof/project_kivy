3601966be5b983933feee2f2eaeb30bd7135e593
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-06-03 16:28:41 +0300

    BoxLayout

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
  Предназначено для удобного составления списка покупок
 '''
 
-
 from kivy.app import App
 from kivy.properties import ColorProperty
 from kivy.uix.button import Button
@@ -11,30 +10,21 @@ from kivy.config import Config
 
 from kivy.uix.floatlayout import FloatLayout
 
+from kivy.uix.boxlayout import BoxLayout
+
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '300')
 Config.set('graphics', 'height', '610')
 
 
-class MyApp(App):
+class ListApp(App):
     def build(self):
-        fl = FloatLayout(size=(150, 150))
-        fl.add_widget(
-            Button(
-                text="Кнопка",
-                font_size=16,
-                on_press=self.pushbutton,
-                background_color=[.42, .32, .55, 1],
-                background_normal='',
-                size_hint = (.5, .25),
-                pos = (0, 0)))
-        return fl
-
-    def pushbutton(self, instance):
-        print('Кнопка нажата')
-        instance.text = "Кнопка нажата"
-
+        menu_list = BoxLayout()
+        menu_list.add_widget(Button(text='1'))
+        menu_list.add_widget(Button(text='1'))
+        menu_list.add_widget(Button(text='1'))
+        return menu_list
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    ListApp().run()
