5216d29e05b95c8d747d0df04231c7f3161233fa
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-06-03 20:13:47 +0300

    GridLayout

--- main.py
+++ main.py
@@ -4,27 +4,30 @@
 '''
 
 from kivy.app import App
-from kivy.properties import ColorProperty
 from kivy.uix.button import Button
 from kivy.config import Config
 
-from kivy.uix.floatlayout import FloatLayout
-
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
 
-Config.set('graphics', 'resizable', '0')
+# Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '300')
 Config.set('graphics', 'height', '610')
 
 
 class ListApp(App):
     def build(self):
-        menu_list = GridLayout(cols=6, padding=[0,250,0,0])
+        main_list = AnchorLayout(anchor_y='bottom')
+        main_list.add_widget(Button(text='List body',
+                                    background_color=[.60,.38,.71,1],
+                                    background_normal=''))
+
+        menu_list = GridLayout(cols=6, spacing=1, size_hint=[None, None], size=[300, 300])
 
         for x in range(36):
             menu_list.add_widget(Button(text='ok'))
-
-        return menu_list
+        main_list.add_widget(menu_list)
+        return main_list
 
 
 if __name__ == "__main__":
