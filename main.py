cd087c0d50df6557a976d8410a348ed932af2bef
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-06-03 22:30:07 +0300

    GridLayout

--- main.py
+++ main.py
@@ -8,27 +8,24 @@ from kivy.uix.button import Button
 from kivy.config import Config
 
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 # Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '300')
 Config.set('graphics', 'height', '610')
 
 
-class ListApp(App):
+class ListPayApp(App):
     def build(self):
-        main_list = AnchorLayout(anchor_y='bottom')
-        main_list.add_widget(Button(text='List body',
-                                    background_color=[.60,.38,.71,1],
-                                    background_normal=''))
+        show_list = BoxLayout(orientation='vertical', padding=5)  # мониторчик с выводом списка
+        show_list.add_widget(Label(text='Text', font_size=20, size_hint=[1, .50]))
+        menu_list = GridLayout(cols=5, padding=2, spacing=1, size_hint=[1, .50])  # кнопки с наименованиями
+        for x in range(20):
+            menu_list.add_widget(Button(background_color=[.20, .35, .25, 1]))
 
-        menu_list = GridLayout(cols=6, spacing=1, size_hint=[None, None], size=[300, 300])
-
-        for x in range(36):
-            menu_list.add_widget(Button(text='ok'))
-        main_list.add_widget(menu_list)
-        return main_list
+        return menu_list
 
 
 if __name__ == "__main__":
-    ListApp().run()
+    ListPayApp().run()
