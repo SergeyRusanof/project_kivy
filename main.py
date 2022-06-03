7231b82e7aa50f51302c65e1597031eadcdb4dbc
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-06-03 16:58:38 +0300

    GridLayout

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from kivy.config import Config
 
 from kivy.uix.floatlayout import FloatLayout
 
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 
 Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '300')
@@ -19,10 +19,11 @@ Config.set('graphics', 'height', '610')
 
 class ListApp(App):
     def build(self):
-        menu_list = BoxLayout()
-        menu_list.add_widget(Button(text='1'))
-        menu_list.add_widget(Button(text='1'))
-        menu_list.add_widget(Button(text='1'))
+        menu_list = GridLayout(cols=6, padding=[0,250,0,0])
+
+        for x in range(36):
+            menu_list.add_widget(Button(text='ok'))
+
         return menu_list
 
 
