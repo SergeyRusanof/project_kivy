cb99b4f28922e8e4177fbd7c8c5541351087e562
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-06-04 22:06:53 +0300

    new_version

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
-'''
-Приложение "Список".
- Предназначено для удобного составления списка покупок
-'''
-
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.config import Config
-
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 # Config.set('graphics', 'resizable', '0')
-Config.set('graphics', 'width', '300')
-Config.set('graphics', 'height', '610')
+Config.set('graphics', 'width', 300)
+Config.set('graphics', 'height', 610)
 
 
-class ListPayApp(App):
+class ShopListApp(App):
     def build(self):
-        show_list = BoxLayout(orientation='vertical', padding=5)  # мониторчик с выводом списка
-        show_list.add_widget(Label(text='Text', font_size=20, size_hint=[1, .50]))
-        menu_list = GridLayout(cols=5, padding=2, spacing=1, size_hint=[1, .50])  # кнопки с наименованиями
-        for x in range(20):
-            menu_list.add_widget(Button(background_color=[.20, .35, .25, 1]))
+        box = BoxLayout(orientation='vertical', size_hint=[1, .40])
+        box.add_widget(Label(text="Приветик) тут будет список", size_hint=[1, .40]))
+        icons = GridLayout(rows=5, padding=2, size_hint=[1, .60])
 
-        return menu_list
+        for i in range(20):
+            icons.add_widget(Button(background_color=[.20, .35, .25, 1]))
+        return box
 
 
 if __name__ == "__main__":
-    ListPayApp().run()
+    ShopListApp().run()
