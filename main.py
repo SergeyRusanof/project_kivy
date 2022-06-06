f5b0236aaa562fc6d165c99546247d080f97a43d
SergeyRusanof <104849267+SergeyRusanof@users.noreply.github.com>
2022-06-06 15:44:05 +0300

    new_version2 + Icons

--- main.py
+++ main.py
@@ -11,18 +11,24 @@ Config.set('graphics', 'height', 610)
 
 
 class ShopListApp(App):
-    def choise_pay(self, instance):
-    def build(self):
+    def update_label(self):
+        self.label.text = self.inlist
 
+    def choise_pay(self, instance):
+        self.inlist += str(instance.text)
+        self.update_label()
+
+    def build(self):
+        self.inlist = ""
         box = BoxLayout(orientation='vertical', padding=2)
         icons = GridLayout(rows=5, padding=2, spacing=1, size_hint=[1, .50])
-        box.add_widget(Label(text="Приветик) тут будет список",
-                             text_size=(294, 605 * .5), font_size=14,
-                             size_hint=[1, .50],
-                             halign='left',
-                             valign='top'))
-        for i in range(20):
-            icons.add_widget(Button(background_color=[.20, .35, .25, 1], on_press = self.choise_pay))
+        self.label = Label(text="Приветик) тут будет список",
+                           text_size=(294, 605 * .5), font_size=14,
+                           size_hint=[1, .50],
+                           halign='left',
+                           valign='top')
+        box.add_widget(self.label)
+        icons.add_widget(Button(img='/prod_img/cor.png', background_color=[.20, .35, .25, 1], on_press=self.choise_pay))
         box.add_widget(icons)
         return box
 
